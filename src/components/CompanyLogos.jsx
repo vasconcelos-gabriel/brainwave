58400c5cacbbf3eecd45beb3c03cbf2592927ecd
vasconcelos-gabriel <vasconcelos.crvg@gmail.com>
2024-03-06 21:37:31 -0300

    hero finalizada

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,10 +1,21 @@
-const CompanyLogos = ({ classNames }) => {
+import { companyLogos } from "../constants";
+
+const CompanyLogos = ({ className }) => {
   return (
-    <div className={classNames}>
-      CompanyLogos
+    <div className={className}>
       <h5 className="tagline mb-6 text-center text-n-1/50">
         HELPING PEOPLE CREATE BEAUTIFUL CONTENT AT
       </h5>
+      <ul className="flex">
+        {companyLogos.map((logo, index) => (
+          <li
+            className="flex h-[8.5rem] flex-1 items-center justify-center"
+            key={index}
+          >
+            <img src={logo} alt={logo} width={134} height={28} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
